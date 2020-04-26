a473d1d02eefc974c801f98c5f29f9b5745e0718
facunava <facunava92@gmail.com>
2020-04-26 13:28:57 -0300

    Agrego TP6

---
+++ Practico_6/TransformacionDeSimilaridad.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+#A. Crear un método nuevo que aplique una transformación euclidiana, recibiendo los siguientes parámetros:
+#Parámetros
+#    * angle: Ángulo
+#    * tx: traslación en x
+#    * ty: traslación en y
+#    Recordar que la transformación euclidiana tiene la siguiente forma:
+#        [cos(angle)  sin(angle) tx]
+#        [-sin(angle) cos(angle) ty]
+#B. Usando como base el programa anterior, escriba un programa que:
+#permita seleccionar un rectángulo de una imagen y con la letra “e” aplique una transformación 
+#euclidiana a la imagen dentro del rectángulo y la guarde en el disco y sale.
+
+
+import cv2
+from fn_transform import euclidean
+from fn_transform import similarity
+
+xi, yi, xf, yf = 0, 0, 0, 0
+angle, tx, ty = 30, 80, 80
+scale = 2.0
+drawing = False
+
+def crop_image(event, x, y, flags, param):
+    w = 2
+    global xi, yi, xf, yf, drawing, img, width
+    if (event == cv2.EVENT_LBUTTONDOWN):
+        xi, yi = x, y
+        drawing = True
+
+    elif (event == cv2.EVENT_MOUSEMOVE):
+        if (drawing):
+            img = roi.copy()
+            cv2.rectangle(img, (xi, yi), (x,y), (255, 0, 0), w)
+
+    elif (event == cv2.EVENT_LBUTTONUP):
+        drawing = False
+        if(x < 0): x = 0 
+        if(y < 0): y = 0
+        xf, yf = x, y   
+
+
+img = cv2.imread('python.jpg', cv2.IMREAD_COLOR)
+xi, yi = 0, 0
+xf, yf = img.shape[1], img.shape[0]
+backup = img.copy()
+roi = img.copy()
+
+cv2.namedWindow('Practico 6')
+cv2.setMouseCallback('Practico 6', crop_image, img)
+
+
+while(True):
+    cv2.imshow('Practico 6', img)
+    option = cv2.waitKey(1) & 0b11111111    #Enmascaro con una AND
+    if option == ord('r'):
+        img = backup.copy()
+        roi = backup.copy()
+        xi, yi = 0, 0
+        xf, yf = img.shape[1], img.shape[0]
+
+    elif option == ord('g'):
+        xi, xf = min(xi, xf), max(xi, xf)
+        yi, yf = min(yi, yf), max(yi, yf)
+        roi = backup[yi:yf, xi:xf]
+        img = roi.copy()
+        cv2.imwrite('python_crop.png', roi)
+    
+    elif option == ord('e'):
+        roi = euclidean(img, angle, tx, ty)
+        img = roi.copy()
+        cv2.imwrite('python_euclidean.png', roi)
+
+    elif option == ord('s'):
+        roi = similarity(img, angle, tx=40, ty=40, scale=1.8)
+        img = roi.copy()
+        cv2.imwrite('python_similarity.png', roi)
+    
+    elif option == ord('q'):
+        break
---
+++ Practico_6/fn_transform.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+
+import cv2
+import numpy as np
+
+
+def euclidean(image, angle,tx=0, ty=0):
+    (h, w) = image.shape[:2]
+    angle = np.radians(angle)   #Giro antihorario
+
+    E = np.float32([[np.cos(angle), np.sin(angle), tx],
+                    [-np.sin(angle), np.cos(angle), ty],
+                    ])
+
+
+    rotated = cv2.warpAffine(image, E, (w, h))
+    return rotated
+
+def similarity(image, angle,tx=0, ty=0, scale=1.0):
+    (h, w) = image.shape[:2]
+    angle = np.radians(angle)   #Giro antihorario
+
+    S = np.float32([[scale*np.cos(angle),  scale*np.sin(angle), tx],
+                    [-scale*np.sin(angle), scale*np.cos(angle), ty],
+                    ])
+
+
+    rotated = cv2.warpAffine(image, S, (w, h))
+    return rotated
