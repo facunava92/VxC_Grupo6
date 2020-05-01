0be593d0724ddb0fe4ce1859a38126f8c9fabec3
facunava <facunava92@gmail.com>
2020-05-01 03:32:16 -0300

    Agrego TP8 empezado

---
+++ Practico_8/RectificandoImagenes.py
@@ -0,0 +1,12 @@
+#/usr/bin/python
+#Teniendo en cuenta que:
+#una homografía se representa con una matriz de 3 × 3 (pero tiene sólo 8 grados delibertad) y puede ser recuperada con 4 puntos no colineales.
+#   A. Crear un método que compute la homografía entre los 4 puntos seleccionados y las esquinas de la segunda imagen de m × n pixeles.
+#   B. Usando como base el programa anterior, escriba un programa que
+#     * con la letra “h” permita seleccionar con el mouse 4 puntos no colineales en una imagen y transforme (rectifique) la selección en 
+#        una nueva imagen rectangular.
+#   Ayuda
+#           cv2.getPerspectiveTransform
+#           cv2.warpPerspective
+
+
